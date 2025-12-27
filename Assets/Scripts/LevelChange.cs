066855d30cc09a770d32e961a79c5677de6fb598
Egorius1268 <dengi.mne.plati1@gmail.com>
2025-12-27 06:47:07 +0300

    some level changes

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class LevelChange : MonoBehaviour
 {
-    public int levelLoadIndex;
+
+    private int levelLoadIndex;
+    private void Start()
+    {
+        levelLoadIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(levelLoadIndex, LoadSceneMode.Single);
+            SceneManager.LoadScene(levelLoadIndex + 1, LoadSceneMode.Single);
         }
     }
 }
